d2eebaf8b55713fec934cd1338912f7c3979bf95
codeexpert254 <masaikipkorir@gmail.com>
2020-04-12 11:17:00 -0700

    estimator changes

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input,
+        inspect: {},
+        severeImpact: {},
+    };
+};
 
 export default covid19ImpactEstimator;
+const covid19data = {
+    region: {
+    name: "Africa",
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+    };
+
+    function currentlyInfected (reportedCases, number){
+        return reportedCases * number;
+    }
+    let reportedCases = 674;
+    let number = 10;
+    console.log(currentlyInfected(reportedCases, number));
+

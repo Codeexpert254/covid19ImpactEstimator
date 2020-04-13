d278049b5c7b2fe2da2b92d45441800380a62caa
codeexpert254 <masaikipkorir@gmail.com>
2020-04-13 14:35:45 -0700

    Changes.

--- src/estimator.js
+++ src/estimator.js
@@ -1,55 +1,56 @@
 const covid19ImpactEstimator = (data) => data;
 module.exports = covid19ImpactEstimator;
 const info = {
-region: {
-name: "Africa",
-avgAge: 19.7,
-avgDailyIncomeInUSD: 5,
-avgDailyIncomePopulation: 0.71
-},
-periodType: "days",
-timeToElapse: 58,
-reportedCases: 674,
-population: 66622705,
-totalHospitalBeds: 1380614
+    'region': {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+        },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
 };
 
-function currentlyInfected(myData){
-return (impactCurrentlyAffected = myData['reportedCases'])*10;
+function currentlyInfected(myData) {
+    return (impact_currentlyAffected = myData['reportedCases']) * 10;
 }
-function currentlyInfectedsevereImpact(myData){
-return (severImpactCurrentlyInfected = myData['reportedCases'])*50;
+function currentlyInfectedsevereImpact(myData) {
+    return (severImpact_currentlyInfected = myData['reportedCases']) * 50;
 }
 
 function infectionsByRequestedTimeCurrentlyInfected(myData) {
-    return (currentlyInfected(myData)* Math.pow(2,9))
+    return (currentlyInfected(myData) * (2 ** 9))
 }
 
 function infectionsByRequestedTimeSevereImpact(myData) {
-    return (currentlyInfectedsevereImpact(myData)* Math.pow(2,9))
+    return (currentlyInfectedsevereImpact(myData)* (2 ** 9))
 }
 
-function returnFinalData(anyData){
-let finalData = {
-"data":anyData,
-"estimates":{
-"currentlyInfected":{
-"currentlyInfected":currentlyInfected(anyData)
-},
-"severeImpactCurrentlyInfected":{
-"severeImpactCurrentlyInfected":currentlyInfectedsevereImpact(anyData)
-},
-"infectionsByRequestedTimeCurrentlyInfected": {
-    "infectionsByRequestedTimeCurrentlyInfected": infectionsByRequestedTimeCurrentlyInfected(anyData)
-},
-"infectionsByRequestedTimeSevereImpact": {
-    "infectionsByRequestedTimeSevereImpact": infectionsByRequestedTimeSevereImpact(anyData)
+
+function returnFinalData (anyData) {
+let FinalData = {
+'data':anyData ,
+'estimates' : {
+'currentlyInfected' : {
+'currentlyInfected': currentlyInfected(anyData)
+            },
+            'severeImpactCurrentlyInfected' : {
+                'severeImpactCurrentlyInfected' : currentlyInfectedsevereImpact(anyData)
+            },
+            'infectionsByRequestedTimeCurrentlyInfected' : {
+                'infectionsByRequestedTimeCurrentlyInfected' : infectionsByRequestedTimeCurrentlyInfected(anyData)
+            } ,
+            'infectionsByRequestedTimeSevereImpact' : {
+                'infectionsByRequestedTimeSevereImpact' : infectionsByRequestedTimeSevereImpact(anyData)
 }
 }
 }
-return finalData;
+return FinalData ;
 }
 
-console.log(returnFinalData(info))
-console.log(returnFinalData(info))
+console.log(returnFinalData(info));
+console.log(returnFinalData(info));
 
